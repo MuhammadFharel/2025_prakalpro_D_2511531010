3c8e5098ea8eb45069db6107d7ee2ac84e62237b
MuhammadFharel <mfharell2222@gmail.com>
2025-11-12 15:10:52 +0700

    Add files via upload

---
+++ 2025_prakalpro_D_2511531010/src/pekan7_2511531010/PanggilMahasiswa_2511531010.java
@@ -0,0 +1,12 @@
+package pekan7_2511531010;
+
+public class PanggilMahasiswa_2511531010 {
+	public static void main (String[] args) {
+		Mahasiswa_2511531010 a= new Mahasiswa_2511531010();
+		a.setNim(531010);
+		a.setNama ("Fharel");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+	}
+}
